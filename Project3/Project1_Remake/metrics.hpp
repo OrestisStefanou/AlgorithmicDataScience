e12eda4489e2d68ffa5caaa1d322811d0c456821
leonidasefrem <leonidasefrem@gmail.com>
2021-01-05 00:27:11 +0200

    getImgsCluster fix

--- Project3/Project1_Remake/metrics.hpp
+++ Project3/Project1_Remake/metrics.hpp
@@ -1,29 +1,28 @@
 #ifndef METRICS_H
 #define METRICS_H
-#include<vector>
-#include <bits/stdc++.h> 
-#include<string.h>
-#include"EMD.hpp"
-#include<math.h>
+#include <vector>
+#include <bits/stdc++.h>
+#include <string.h>
+#include "EMD.hpp"
+#include <math.h>
 
 using namespace std;
 
-// Driver function to sort the vector elements 
-// by second element of pairs 
-bool sortbysec(const pair<int,int> &a,const pair<int,int> &b) 
-{ 
-    return (a.second < b.second); 
-} 
+// Driver function to sort the vector elements
+// by second element of pairs
+bool sortbysec(const pair<int, int> &a, const pair<int, int> &b)
+{
+    return (a.second < b.second);
+}
 
 //Class where we hold different kind of LSH-able metrics
 class Metrics
 {
 private:
-
 public:
     Metrics();
     ~Metrics();
-    int get_distance(vector<double> a,vector<double> b,char *type);
+    int get_distance(vector<double> a, vector<double> b, char *type);
     //Add more distance metrics here
 };
 
@@ -38,15 +37,18 @@ Metrics::~Metrics()
 //Returns the "type" distance between vector a and vector b
 //type = "L1"->returns Manhatan distance
 //type = "EMD"->returns Earth mover's distance
-int Metrics::get_distance(vector<double> a,vector<double> b,char *type){
+int Metrics::get_distance(vector<double> a, vector<double> b, char *type)
+{
     int distance = 0;
 
-    if(a.size()!=b.size()){
+    if (a.size() != b.size())
+    {
         cout << "Size of two vectors is not the same" << endl;
         return -1;
     }
     //Manhattan distance
-    if(strcmp((char *)"L1",type)==0){
+    if (strcmp((char *)"L1", type) == 0)
+    {
         for (int i = 0; i < a.size(); i++)
         {
             distance += abs(double(a[i]) - double(b[i]));
@@ -54,7 +56,8 @@ int Metrics::get_distance(vector<double> a,vector<double> b,char *type){
         return distance;
     }
     //Earth mover's distance
-    if(strcmp((char *)"EMD",type) == 0){
+    if (strcmp((char *)"EMD", type) == 0)
+    {
         vector<double> aw;
         vector<double> bw;
         //Initialize weights(We give the same weight to each pixel)
@@ -63,40 +66,60 @@ int Metrics::get_distance(vector<double> a,vector<double> b,char *type){
             aw.push_back(10);
             bw.push_back(10);
         }
-        distance = emd(a,aw,b,bw);
+        distance = emd(a, aw, b, bw);
         return distance;
     }
     return distance;
 }
 
 //Creates a new dataset in imgsCluster vector with a cluster of size dim for every image
-void getImgsCluster(vector<vector<double>> &images,int dim,vector<vector<double>> &imgsCluster){
-    imgsCluster.resize(images.size(), vector<double>(dim * dim)); //Resize the vector
+void getImgsCluster(vector<vector<double>> &images, int dim, vector<vector<double>> &Centroids, vector<vector<double>> &Weights)
+{
+    Centroids.resize(images.size(), vector<double>(dim * dim)); //Resize the vector
+    Weights.resize(images.size(), vector<double>(dim * dim));   //Resize the vector
     int originalDim = int(sqrt(images[0].size()));
-    int startClusterIndex = (originalDim - dim)/2;
-    //cout << originalDim << endl;
-    //cout << startClusterIndex << endl;
+    int newImageDim = int(sqrt(dim));
+    int PixelsPerCluster = originalDim / newImageDim;
+    int centroid = ((PixelsPerCluster ^ 2) / 2) + 1;
     for (int i = 0; i < images.size(); i++)
     {
-        int clusterColIndex = (startClusterIndex * originalDim) + startClusterIndex;
+        int clusterColIndex = 0;
+        int clusterRowIndex = 0;
+        int RowCounter = newImageDim - 1;
         for (int j = 0; j < dim; j++)
         {
-            
-            for (int k = 0; k < dim; k++)
+            double ClusterSign = 0;
+            int centroidCounter = 0;
+            for (int k = clusterRowIndex; k < PixelsPerCluster + clusterRowIndex; k++)
             {
-                int colIdx = (j + k) + (j*(dim-1));
-                //cout << colIdx << " ";
-                imgsCluster[i][colIdx] = images[i][clusterColIndex];
-                clusterColIndex++;
+                for (int m = clusterColIndex; m < PixelsPerCluster + clusterColIndex; m++)
+                {
+                    ClusterSign = ClusterSign + images[i][(PixelsPerCluster * k) + m];
+                    if (centroidCounter == centroid)
+                    {
+                        Centroids[i][j] = images[i][(PixelsPerCluster * k) + m];
+                    }
+                }
             }
-            //cout << endl;
-            clusterColIndex = (startClusterIndex * originalDim) + ((j+1) * originalDim) + startClusterIndex;
+            if (RowCounter == 0)
+            {
+                RowCounter = newImageDim - 1;
+                clusterRowIndex = clusterRowIndex + PixelsPerCluster;
+                clusterColIndex = 0;
+            }
+            else
+            {
+                clusterColIndex = clusterColIndex + PixelsPerCluster;
+                RowCounter--;
+            }
+            Weights[i][j] = ClusterSign;
         }
     }
 }
 
 //Returns a pair with number of correct and wrong predictions
-pair<int,int> compareResults(vector<int> &trainLabels,vector<int> &testLabels,vector<pair<int, int>> &results,int queryIndex){
+pair<int, int> compareResults(vector<int> &trainLabels, vector<int> &testLabels, vector<pair<int, int>> &results, int queryIndex)
+{
     int correct = 0;
     int wrong = 0;
     int queryLabel = testLabels[queryIndex];
@@ -105,14 +128,15 @@ pair<int,int> compareResults(vector<int> &trainLabels,vector<int> &testLabels,ve
         if (trainLabels[results[i].first] == queryLabel)
         {
             correct++;
-        }else
+        }
+        else
         {
             wrong++;
         }
     }
     //cout << correct << " correct neighbors found" << endl;
     //cout << wrong << " wrong neighbors found" << endl;
-    return make_pair(correct,wrong);
+    return make_pair(correct, wrong);
 }
 
 #endif
